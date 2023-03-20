179c750dcfa45a5cd8866078fa09f4fff7f9080a
lidevlin <lidevlin95@gmail.com>
2023-03-20 16:07:28 +0000

    Finalized

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -33,10 +33,11 @@ const About = () => {
           in frameworks like Nextjs and Astro. I'm a quick learner and
           collaborate closely with clients to create efficient, scalable, and
           user-friendly solutions that solve real-world problems. Let's work
-          together to bring your ideas to life! In my personal life I play chess, active member 
-          in a monthly bookclub and keep up to date with interesting technical projects.
+          together to bring your ideas to life! In my personal life I play
+          chess, active member in a monthly bookclub and keep up to date with
+          interesting technical projects.
         </motion.p>
-        {/* <div className="mx-auto mt-20 flex flex-wrap gap-10">
+        <div className="mx-auto mt-20 flex flex-wrap gap-10">
           {ServiceCardList.map((service, index) => (
             <ServiceCard
               key={service.index}
@@ -45,7 +46,7 @@ const About = () => {
               icon={service.icon}
             />
           ))}
-        </div> */}
+        </div>
       </div>
     </div>
   );
@@ -54,41 +55,51 @@ const About = () => {
 const ServiceCardList = [
   {
     index: 1,
-    title: "Web Development",
-    icon: "/images/web-development.svg",
+    title: "Chess",
+    icon: "/images/assets/Chess.png",
   },
   {
     index: 2,
-    title: "Mobile Development",
-    icon: "/images/mobile-development.svg",
+    title: "Reading",
+    icon: "/images/assets/book.png",
   },
   {
     index: 3,
-    title: "UI/UX Design",
-    icon: "/images/ui-ux-design.svg",
-  },
-  {
-    index: 4,
-
-    title: "Digital Marketing",
-
-    icon: "/images/digital-marketing.svg",
+    title: "Tech Projects",
+    icon: "/images/assets/Tech.png",
   },
 ];
 
-export const ServiceCard = ({ index, title, icon }: any) => (
-  <Tilt className="">
-    <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className="green-pink-gradient shadow-card' w-full rounded-[20px] p-[1px]"
+export const ServiceCard = ({ index, title, icon }: any) => {
+  return (
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="w-[10rem] rounded-2xl p-5 md:w-[20rem] "
     >
-      <div className="flex min-h-[280px] flex-col items-center justify-evenly rounded-[20px] bg-[#030714] py-5 px-12">
-        <h3 className="text-center text-[20px] font-bold text-white">
-          {title}
-        </h3>
-      </div>
-    </motion.div>
-  </Tilt>
-);
+      <motion.div
+        variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+        className="green-pink-gradient shadow-card' h-full w-full rounded-[20px] p-[1px] md:min-h-[280px]"
+      >
+        <div className="flex h-full flex-col items-center justify-evenly rounded-[20px] bg-[#030714] py-2 md:min-h-[280px] ">
+          <div className="relative w-full md:h-[230px]">
+            <img
+              src={icon}
+              alt="project_image"
+              className="h-full w-full rounded-2xl px-2"
+            />
+          </div>
+
+          <div className="mt-2 px-12">
+            <h3 className="text-[24px] font-bold text-white">{title}</h3>
+          </div>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 export default SectionWrapper(About, "About");
--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -56,7 +56,8 @@ const Tech = () => {
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          and manage projects effectively. Check some of my projects out at the
+          bottom of the page.
         </motion.p>
       </div>
 
